41ec33c2b1200d077f2329238c80da4a718339ac
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-04 17:36:50 +0530

    [Vaibhav] Add: UC_01

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UC_01
+    {
+        class Program
+        {
+        static void Main(string[] args) 
+            {
+                //Constants
+                int Present = 1;
+                Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Present) 
+                {
+                    Console.WriteLine("Employee is Present");
+                }
+            else
+                { 
+                    Console.WriteLine("Employee is Absent");
+                }
+            } 
+        }
+    }
+
