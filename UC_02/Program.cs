47f53338f587c8c193e781825a4abaa031ebb6c7
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-04 18:04:28 +0530

    [Vaibhav] Add: UC_02

---
+++ UC_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UC_02
+{
+    class Program
+    {
+           static void Main(string[] args)
+        {
+            //Constant
+            int Full_Time = 1;
+            int Emp_Rate_per_Hour = 20;
+            //Variables
+            int empHours = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Full_Time) 
+            {
+                empHours = 8;
+            }
+            else
+            {
+                empHours = 0;
+            }
+            empWage = empHours * Emp_Rate_per_Hour;
+            Console.WriteLine("Employee Wage: " + empWage);
+        }
+    }
+}
